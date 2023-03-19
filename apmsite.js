3aa78cd9c9df88d98850ae57086a2405cfe78116
knottree <125846114+knottree@users.noreply.github.com>
2023-03-19 18:52:32 -0400

    updated track numbers

--- apmsite.js
+++ apmsite.js
@@ -1,6 +1,6 @@
 const skillNames = ["Consciously Limited Empathy","Prioritization","Compatibility","Adding Subtraction","Should You Ask Yes/No Questions?"];
 const skillScriptFileNames = ["consciouslyLimitedEmpathy","prioritization","compatibility","addingSubtraction","shouldYouAskYesNoQuestions"];
-const skillTrackNum = ["1450036405","1450036396","1450036414","1450036423","1450036381"];
+const skillTrackNum = ["1472696008","1472695999","1472695981","1472695963","1472695954"];
 const numSkills = skillTrackNum.length;
 let skillIdx;
 let todaySkillFileName;
@@ -24,7 +24,7 @@ todaySkill = `<iframe width="100%" height="150" scrolling="no" frameborder="no" 
 todaySkillScriptFile = skillScriptFileNames[skillIdx];
 todaySkillScript = `<embed type="text/html" src="${todaySkillScriptFile}.html" width="100%" height="25%">`;
 const whatisscript = `<embed type="text/html" src="whatIsAPM.html" width="100%" height="25%">`;
-const whatisepisode = `<iframe width="100%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1450036366&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false"></iframe><div id="whatisscript"></div>`;
+const whatisepisode = `<iframe width="100%" height="150" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1472696020&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false"></iframe><div id="whatisscript"></div>`;
 document.getElementById("todayname").innerText = todaySkillName;
 document.getElementById("todayskill").innerHTML = todaySkill;
 document.getElementById("todayskillscript").innerHTML = todaySkillScript;
